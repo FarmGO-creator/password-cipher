17158d8a616e1bbd5613e2ee1ee11e08c6903e2b
Polushin Dima <dima2004polushin2004@gmail.com>
2023-01-19 13:52:54 +0600

    Подключил reducers к store

--- cipherPass-frontend/src/app/store.ts
+++ cipherPass-frontend/src/app/store.ts
@@ -1,7 +1,12 @@
 import {configureStore} from "@reduxjs/toolkit";
+import {encodeReducer} from "../store/encodeSlice";
+import {decodeReducer} from "../store/decodeSlice";
 
 export const store = configureStore({
-  reducer: {}
+  reducer: {
+    encode: encodeReducer,
+    decode: decodeReducer,
+  }
 });
 
 export type RootState = ReturnType<typeof store.getState>;
