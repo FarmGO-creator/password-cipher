904c3b6b37854074cc590e795671c3fd55e32689
Polushin Dima <dima2004polushin2004@gmail.com>
2023-01-19 13:38:50 +0600

    Описал глобальные типы

---
+++ cipherPass-frontend/src/types.d.ts
@@ -0,0 +1,18 @@
+export interface CipherTypePost {
+  message: string;
+  password: string;
+}
+
+export interface CipherTypeForm {
+  messageE: string;
+  password: string;
+  messageD: string;
+}
+
+export interface Encode {
+  encoded: string;
+}
+
+export interface Decode {
+  decoded: string;
+}
